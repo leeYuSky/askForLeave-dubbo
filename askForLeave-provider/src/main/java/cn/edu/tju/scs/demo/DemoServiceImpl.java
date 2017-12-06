c14434f678405ea05d117333edbead72cceec925
liyuze <409654850@qq.com>
2017-12-06 22:59:46 +0800

    fix

--- askForLeave-provider/src/main/java/cn/edu/tju/scs/demo/DemoServiceImpl.java
+++ askForLeave-provider/src/main/java/cn/edu/tju/scs/demo/DemoServiceImpl.java
@@ -25,7 +25,6 @@ public class DemoServiceImpl implements DemoService {
 
     public String sayHello(String name) {
         System.out.println("------------------------------------------------");
-        System.out.println("------------------------------------------------");
         System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name + ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
         return "Hello " + name + ", response form provider: " + RpcContext.getContext().getLocalAddress();
     }
